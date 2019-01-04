9318fd617f19a1872e51207630c5d51d6b7cf8e6
igor <iluchenkov@gmail.com>
2019-01-04 03:32:41 +0300

    change confirm message

--- src/components/Menu/confirm.js
+++ src/components/Menu/confirm.js
@@ -4,7 +4,7 @@ export default function Confirm({ onClick }) {
   return (
     <div onClick={onClick} className="Menu-confirm">
       <div>
-        Open fullscreen <small>[F11]</small> & Click me!
+        Open fullscreen <small>[F11]</small> & click here!
       </div>
     </div>
   )
