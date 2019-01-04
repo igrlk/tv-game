e7b5e80d7c32c3e00ec5bbd2999e7ed1716cf604
igor <iluchenkov@gmail.com>
2019-01-04 21:25:52 +0300

    add redux-thunk

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,12 +1,20 @@
 export const IS_SOUND_MUTED_TOGGLED = 'app/IS_SOUND_MUTED_TOGGLED'
+export const INTRO_WAS_PLAYED = 'app/INTRO_WAS_PLAYED'
 
 const initialState = {
-  isSoundMuted: false
+  isSoundMuted: false,
+  isIntroWasPlayed: false
 }
 
 export function toggleIsSoundMuted() {
-  return dispatch => {
+  return async dispatch => {
     dispatch({ type: IS_SOUND_MUTED_TOGGLED })
+  }
+}
+
+export function setIntroWasPlayed(value) {
+  return async dispatch => {
+    dispatch({ type: INTRO_WAS_PLAYED, value })
   }
 }
 
@@ -14,6 +22,8 @@ export default function gameReducer(state = initialState, action) {
   switch (action.type) {
     case IS_SOUND_MUTED_TOGGLED:
       return { ...state, isSoundMuted: !state.isSoundMuted }
+    case INTRO_WAS_PLAYED:
+      return { ...state, isIntroWasPlayed: action.value }
 
     default:
       return state
