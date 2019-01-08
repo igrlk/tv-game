b2528875926cfe91247665a74652edc4df218f65
igor <iluchenkov@gmail.com>
2019-01-09 02:28:23 +0300

    refactoring

--- src/components/common/button.js
+++ src/components/common/button.js
@@ -1,42 +1,17 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-import hoverAudio from '../../assets/sounds/menu/onhover.mp3'
+import SoundsPlayer from '../../helpers/sounds-player'
 
-class Button extends React.Component {
-  hoverAudio = React.createRef()
-
-  hoverHandler = () => {
-    if (!this.props.isSoundMuted) {
-      const hoverAudio = new Audio(this.hoverAudio.current.src)
-      hoverAudio.volume = 0.4
-      hoverAudio.play()
-    }
-  }
-
-  render() {
-    const { children, onClick } = this.props
-    return (
-      <React.Fragment>
-        <button onClick={onClick} onMouseOver={this.hoverHandler}>
-          {children}
-        </button>
-
-        <audio
-          volume="0.1"
-          ref={this.hoverAudio}
-          src={hoverAudio}
-          preload="true"
-        />
-      </React.Fragment>
-    )
-  }
+export default function Button({ children, onClick }) {
+  return (
+    <button
+      onClick={() => {
+        SoundsPlayer.play('click')
+        onClick()
+      }}
+      onMouseOver={() => SoundsPlayer.play('hover', 0.4)}
+    >
+      {children}
+    </button>
+  )
 }
-
-const mapStateToProps = ({ app }) => {
-  return {
-    isSoundMuted: app.isSoundMuted
-  }
-}
-
-export default connect(mapStateToProps)(Button)
