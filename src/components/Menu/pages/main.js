fb8027a9e693287551bfcd4404b442a49b0383c5
igor <iluchenkov@gmail.com>
2019-01-09 02:28:42 +0300

    add username changing

--- src/components/Menu/pages/main.js
+++ src/components/Menu/pages/main.js
@@ -3,8 +3,27 @@ import React from 'react'
 import Button from '../../common/button'
 
 export default class MenuMainPage extends React.Component {
+  state = {
+    isUsernameIsEditing: false,
+    username: ''
+  }
+
+  onChangeHandler = ({ target }) => {
+    const { value } = target
+    if (value.length > 0) {
+      this.setState({ username: target.value })
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      username: this.props.username
+    })
+  }
+
   render() {
-    const { username, history, changeCurrentPage } = this.props
+    const { setUsername, history, changeCurrentPage } = this.props
+    const { isUsernameIsEditing, username } = this.state
     return (
       <div className="Menu-main-page Menu-centered">
         <h1>
@@ -12,11 +31,37 @@ export default class MenuMainPage extends React.Component {
         </h1>
 
         <div className="Menu-main-page__username">
+          {isUsernameIsEditing && (
+            <input
+              maxLength="20"
+              type="text"
+              value={username}
+              onChange={this.onChangeHandler}
+            />
+          )}
           <h2>{username}</h2>
-          <Button onClick={() => console.log('clicked')}>
-            change username
-          </Button>
-          <div>(section is under development)</div>
+          {isUsernameIsEditing ? (
+            <Button
+              onClick={() => {
+                this.setState({
+                  isUsernameIsEditing: false
+                })
+                setUsername(username)
+              }}
+            >
+              save username
+            </Button>
+          ) : (
+            <Button
+              onClick={() =>
+                this.setState({
+                  isUsernameIsEditing: true
+                })
+              }
+            >
+              change username
+            </Button>
+          )}
         </div>
 
         <Button onClick={() => history.push('/game')}>new game</Button>
